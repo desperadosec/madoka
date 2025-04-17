0eff1c1fcdc1779f091eda84786333952f2b267e
Desperado Security <admin@desperadosecurity.com>
2025-04-17 16:45:16 -0400

    Changing paths to not have spaces

--- util/mac_mkramdisk.sh
+++ util/mac_mkramdisk.sh
@@ -3,4 +3,4 @@
 #ram://2097152 --> 1 GB
 #ram://4194304 --> 2 GB
 #ram://8388608 --> 4 GB
-diskutil erasevolume HFS+ 'RAM Disk' `hdiutil attach -nobrowse -nomount ram://2097152`
+diskutil erasevolume HFS+ 'ramdisk' `hdiutil attach -nobrowse -nomount ram://2097152`
