c5824c4257dac52c0aa1d00cb3ac08b6ceac9477
Desperado Security <admin@desperadosecurity.com>
2025-04-18 13:59:24 -0400

    Notes for upcoming TODO

--- inputgen.sh
+++ inputgen.sh
@@ -1,3 +1,5 @@
 honggfuzz -i input -x -- ./wrapper ___FILE___
 # This should generate inputs that also crash
+# TODO: Modify the line to save non-unique inputs as well
+# We want to go for speed and volume over any uniqueness
 
