7f818838e464c737e664e1d791c25272868896d6
Desperado Security <admin@desperadosecurity.com>
2025-04-29 08:35:20 -0400

    Changing some command line args

--- inputgen.sh
+++ inputgen.sh
@@ -1,4 +1,4 @@
-honggfuzz -i input -o output -x -- ./wrapper ___FILE___
+honggfuzz -i input --crashdir crashes -x -- ./test/wrapper ___FILE___
 # This should generate inputs that also crash
 # TODO: Modify the line to save non-unique inputs as well
 # We want to go for speed and volume over any uniqueness
